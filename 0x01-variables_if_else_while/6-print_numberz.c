96a55c7c430abb16deda1872686a9ff7de2d0ff7
Praise Jah Rhodes <praisejahix@gmail.com>
2023-03-20 21:49:29 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints single digits 1 to 9
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
