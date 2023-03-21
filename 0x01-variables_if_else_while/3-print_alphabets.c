5eede390ed9fa5be944e51118cf0ce8e6a226cd0
Praise Jah Rhodes <praisejahix@gmail.com>
2023-03-20 21:41:30 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in both lower and uppercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
