feb19e069569c9202f7b5f897a8e802ef3fa8031
Praise Jah Rhodes <praisejahix@gmail.com>
2023-03-26 16:22:38 -0700

    printsquare

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: the size of the square
+ * Return: none
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
