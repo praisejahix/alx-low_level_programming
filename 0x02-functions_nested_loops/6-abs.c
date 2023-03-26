df480d3df394becbb1ac2239c17cdf83b60f8fef
Praise Jah Rhodes <praisejahix@gmail.com>
2023-03-26 12:02:51 -0700

    There is no

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: is the number to be checked
+ * Return: int (Success)
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	return (-n);
+}
+
